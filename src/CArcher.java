29bd208868e7f3c3dda10e4be3bce4a44dcd76a9
Emil Kopytek <emilkopytek@gmail.com>
2023-09-14 14:11:58 +0200

    Each profession can equip special weapon
    It's impossible to equip sword as archer, etc.

--- src/CArcher.java
+++ src/CArcher.java
@@ -1,26 +1,31 @@
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CArcher extends CWarior {
-
     public CArcher(float damage, String name, String profession) {
-        super(damage, name, profession);
-        this.profession = "Archer";
+        super(
+                damage,
+                name,
+                "Archer",
+                new CBow(5f, "Podstawowy Luk", false, true, 5)
+        );
     }
-
     @Override
-    public void makeDamage(CWarior target) {
-        float damage = this.getWeapon() == null ? this.getDamage() : this.getDamage() + this.getWeapon().getDamage();
+    public boolean makeDamage(CWarior target) {
+        float damage = this.getDamage();
+
         int randomNum = ThreadLocalRandom.current().nextInt(0, 10 + 1);
 
-        if(randomNum > this.getWeapon().getDispersion()) {
+        if(randomNum >= this.getWeapon().getDispersion()) {
             target.takeDamage(damage);
+            return true;
         } else {
-            System.out.println("Nie trafiles");
+            this.takeDamage(target.getDamage());
+            System.out.println("Nie trafiles w " + target.getName());
         }
+        return false;
     }
-
     @Override
     public void setWeapon(CWeapon weapon) {
-        if(this.getWeapon() == null) this.setWeapon(weapon);
+        this.weapon = weapon;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
+    public static boolean ifSword(CWeapon x) { return  x.getClass() == CSword.class; }
 
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
 
-        var wojownik1 = new CArcher(10f, "Najwiekszy Wojownik Swiata", "Archer");
-        var wojownik2 = new CArcher(5f, "Najgordszy Wojownik Polski", "Archer");
+        var wojownik1 = new CArcher(10f, "Player", "Archer");
+        var wojownik2 = new CArcher(5f, "Enemy", "Archer");
 
         var sword = new CSword(20f, "Magiczny Miecz", true, false);
-        var bow = new CBow(40f, "Wielki Luk", true, false, 1);
+        var bow = new CBow(12.5f, "Wielki Luk", true, false, 2);
 
         System.out.println("Jestes wojownikiem. Twoje imie to " + wojownik1.getName());
         System.out.println("Walczysz z " + wojownik2.getName());
@@ -21,8 +23,8 @@ public class Main {
 
             System.out.println("Lista czynnosci:");
             System.out.println("1. Bij");
-            System.out.println("2. Podnies bron: " + sword.getName());
-            if(playerWeapon != null)
+            System.out.println("2. Podnies bron: " + bow.getName());
+            if(playerWeapon.getUpgradeable())
             {
                 System.out.println("3. Ulepsz bron: " + playerWeapon.getName());
             }
@@ -31,27 +33,45 @@ public class Main {
 
             switch (option) {
                 case "1":
-                    wojownik1.makeDamage(wojownik2);
-                    System.out.println("Wojownikowi " + wojownik2.getName() + " zostalo " + wojownik2.getHealth());
+                    if(wojownik1.makeDamage(wojownik2))
+                    {
+                        System.out.println("Wojownikowi " + wojownik2.getName() + " zostalo " + wojownik2.getHealth());
+                    } else {
+                        System.out.println("Zostalo ci " + wojownik1.getHealth());
+                    }
                     break;
                 case "2":
-                    wojownik1.setWeapon(sword);
-
-                    System.out.println("Podniesiono bron: " + sword.getName());
-                    System.out.println("Twoje obrazenia wzrosly o " + wojownik1.getWeapon().getDamage() + " i wynosza: " + wojownik1.getDamage());
-                    break;
-                case "3":
-                    if(playerWeapon == null) {
+                    if(bow.getEquipped()) {
+                        System.out.println("Podniosles juz ta bron.");
                         wojownik2.makeDamage(wojownik1);
                         System.out.println("Zostalo ci " + wojownik1.getHealth());
                         break;
                     }
 
+                    if(Objects.equals(wojownik1.getProfession(), "Archer"))
+                    {
+                        if(ifSword(bow))
+                        {
+                            System.out.println("Nie mozesz podniesc miecza, jestes lucznikiem!");
+                            wojownik2.makeDamage(wojownik1);
+                            System.out.println("Zostalo ci " + wojownik1.getHealth());
+                            break;
+                        } else {
+                            wojownik1.setWeapon(bow);
+                            System.out.println("Podniesiono bron: " + bow.getName());
+                            System.out.println("Twoje obrazenia wzrosly o " + wojownik1.getWeapon().getDamage() + " i wynosza: " + wojownik1.getDamage());
+                        }
+                    }
+                    break;
+
+                case "3":
                     if(wojownik1.getWeapon().upgradeWeapon(4.5f)) {
                         System.out.println("Ulepszono bron.");
                         System.out.println("Jej obrazenia wynosza: " + wojownik1.getWeapon().getDamage());
                     } else {
                         System.out.println("Nie mozna ulepszyc broni.");
+                        wojownik2.makeDamage(wojownik1);
+                        System.out.println("Zostalo ci " + wojownik1.getHealth());
                     }
                     break;
 
